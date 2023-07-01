f77a100084946de616baa43988dfc6de48e3bac8
Sir_Uddy <udoka.eth@gmail.com>
2023-07-01 23:53:45 +0100

    Update contracts2.sol

--- contracts/contracts2.sol
+++ contracts/contracts2.sol
@@ -1,1 +1,30 @@
 
+// SPDX-License-Identifier: <License-Identifier>
+// State variables
+pragma solidity ^0.8.0;
+
+contract MySmartContract {
+    
+    uint256 public myVariable;
+
+    // Events
+    event ValueChanged(uint256 newValue);
+
+    // Constructor
+    constructor() {
+        myVariable = 0;
+    }
+
+  // Function to update the value
+    function updateValue(uint256 newValue) public {
+        myVariable = newValue;
+        emit ValueChanged(newValue);
+    }
+
+    // Function to retrieve the current value
+    function getValue() public view returns (uint256) {
+        return myVariable;
+    }
+  
+}
+
